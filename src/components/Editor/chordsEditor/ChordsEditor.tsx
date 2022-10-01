554f6ae35215c38e0f51e0c47fdf8abbbc896d12
Alexander Danilov <alexandr.g.danilov@gmail.com>
2022-10-01 20:19:08 +0300

    Chords instrumental part line array editing

--- src/components/Editor/chordsEditor/ChordsEditor.tsx
+++ src/components/Editor/chordsEditor/ChordsEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { PageContent } from '../../common/PageContent/PageContent';
 import { PageHeader, ButtonInfo } from '@components/common/PageHeader';
 import './ChordsEditor.scss';
--- src/components/Editor/chordsEditor/InstrumentalPart/InstrumentalPart.tsx
+++ src/components/Editor/chordsEditor/InstrumentalPart/InstrumentalPart.tsx
@@ -1,125 +1,26 @@
 import React from 'react';
-import { Chord } from '../components/Chord';
-import './InstrumentalPart.scss';
 import { useEditorContext } from '../EditorContext';
 import { InstrumentalPartType } from '@model/song';
-import { AddChordArea, ChordEditPopup } from '../components';
-import EditIcon from '@rsuite/icons/Edit';
-import { Whisper } from 'rsuite';
-import cn from 'classnames';
-
-const CLASS = 'instrumental-part';
+import { ChordsLine } from './ChordsLine';
 
 export interface InstrumentalPartProps {
     path: string;
 }
 
 export const InstrumentalPart: React.FC<InstrumentalPartProps> = ({ path }) => {
-    const { value: part, dispatch } = useEditorContext(path);
+    const { value: part } = useEditorContext(path);
 
     const { chords, tabs } = part as InstrumentalPartType;
 
     return (
         <div>
             {chords
-                ? chords
-                      .map((line, index) => [
-                          <div
-                              className={cn({
-                                  [`${CLASS}__line--offset`]: !line.length,
-                              })}
-                          >
-                              {!line.length
-                                  ? [
-                                        <Whisper
-                                            placement={'top'}
-                                            trigger={'click'}
-                                            speaker={
-                                                <ChordEditPopup
-                                                    onSubmit={(chord) =>
-                                                        dispatch({
-                                                            type: 'addArrayValue',
-                                                            payload: {
-                                                                path: `${path}.chords[${index}]`,
-                                                                value: chord,
-                                                            },
-                                                        })
-                                                    }
-                                                />
-                                            }
-                                        >
-                                            <EditIcon
-                                                className={`${CLASS}__empty-button`}
-                                            />
-                                        </Whisper>,
-                                    ]
-                                  : [
-                                        <AddChordArea
-                                            key={`${index}_area_first`}
-                                            onAddChord={(chord) =>
-                                                dispatch({
-                                                    type: 'addArrayValue',
-                                                    payload: {
-                                                        path: `${path}.chords[${index}]`,
-                                                        value: chord,
-                                                        index: 0,
-                                                    },
-                                                })
-                                            }
-                                        />,
-                                        ...line.map((chord, chordIndex) => (
-                                            <>
-                                                {/* TODO: нормальный key*/}
-                                                <Chord
-                                                    hasPadding={false}
-                                                    key={`${index}_${chordIndex}`}
-                                                    chord={chord}
-                                                    onEdit={(chord) =>
-                                                        dispatch({
-                                                            type: 'setValue',
-                                                            payload: {
-                                                                path: `${path}.chords[${index}][${chordIndex}]`,
-                                                                value: chord,
-                                                            },
-                                                        })
-                                                    }
-                                                    onRemove={() =>
-                                                        dispatch({
-                                                            type: 'removeArrayValue',
-                                                            payload: {
-                                                                path: `${path}.chords[${index}]`,
-                                                                index: chordIndex,
-                                                            },
-                                                        })
-                                                    }
-                                                />
-                                                <AddChordArea
-                                                    shortArea={
-                                                        chordIndex !=
-                                                        line.length - 1
-                                                    }
-                                                    key={`${index}_area_[${
-                                                        chordIndex + 1
-                                                    }]`}
-                                                    onAddChord={(chord) =>
-                                                        dispatch({
-                                                            type: 'addArrayValue',
-                                                            payload: {
-                                                                path: `${path}.chords[${index}]`,
-                                                                value: chord,
-                                                                index:
-                                                                    chordIndex +
-                                                                    1,
-                                                            },
-                                                        })
-                                                    }
-                                                />
-                                            </>
-                                        )),
-                                    ]}
-                          </div>,
-                      ])
-                      .reduce((prev, next) => [...prev, <br />, ...next])
+                ? chords.map((_, index) => (
+                      <ChordsLine
+                          lineArrayPath={`${path}.chords`}
+                          lineIndex={index}
+                      />
+                  ))
                 : tabs}
         </div>
     );
